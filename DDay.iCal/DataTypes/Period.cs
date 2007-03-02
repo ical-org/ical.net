3574aa8742e38616b3698174ec9c25b9fa62561b
dougday <dougday@105679f1-c027-0410-8e23-b73c1a32fa19>
2007-03-02 17:13:38 +0000

    Converted all public fields into public properties

--- DDay.iCal/DataTypes/Period.cs
+++ DDay.iCal/DataTypes/Period.cs
@@ -11,18 +11,45 @@ namespace DDay.iCal.DataTypes
     [DebuggerDisplay("Period ( {StartTime} - {EndTime} )")]
     public class Period : iCalDataType, IComparable
     {
-        #region Public Fields
+        #region Private Fields
 
-        public Date_Time StartTime = new Date_Time();
-        public Date_Time EndTime;
-        public Duration Duration;
+        private Date_Time m_StartTime = new Date_Time();        
+        private Date_Time m_EndTime;        
+        private Duration m_Duration;
+        private bool m_MatchesDateOnly = false;
+
+        #endregion
+
+        #region Public Properties
+
+        public Date_Time StartTime
+        {
+            get { return m_StartTime; }
+            set { m_StartTime = value; }
+        }
+
+        public Date_Time EndTime
+        {
+            get { return m_EndTime; }
+            set { m_EndTime = value; }
+        }
+
+        public Duration Duration
+        {
+            get { return m_Duration; }
+            set { m_Duration = value; }
+        }
         
         /// <summary>
         /// When true, comparisons between this and other <see cref="Period"/>
         /// objects are matched against the date only, and
         /// not the date-time combination.
         /// </summary>
-        public bool MatchesDateOnly = false;
+        public bool MatchesDateOnly
+        {
+            get { return m_MatchesDateOnly; }
+            set { m_MatchesDateOnly = value; }
+        }
 
         #endregion
 
@@ -39,13 +66,26 @@ namespace DDay.iCal.DataTypes
                 EndTime = end.Copy();
                 Duration = new Duration(end.Value - start.Value);
             }
+            else
+            {
+                EndTime = StartTime;
+                Duration = new Duration(new TimeSpan(0));
+            }
         }
         public Period(Date_Time start, TimeSpan duration)
             : this()
         {
-            StartTime = start.Copy();            
-            Duration = new Duration(duration);            
-            EndTime = start + duration;
+            StartTime = start.Copy();
+            if (duration != TimeSpan.MinValue)
+            {
+                Duration = new Duration(duration);
+                EndTime = start + duration;
+            }
+            else
+            {
+                Duration = new Duration(new TimeSpan(0));
+                EndTime = StartTime;                
+            }
         }
         public Period(string value)
             : this()
@@ -146,7 +186,7 @@ namespace DDay.iCal.DataTypes
                     return 0;
                 else if (StartTime < ((Period)obj).StartTime)
                     return -1;
-                else if (StartTime > ((Period)obj).StartTime)
+                else if (StartTime >= ((Period)obj).StartTime)
                     return 1;
             }
             throw new ArgumentException("obj must be a Period type");
