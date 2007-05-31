e361fcf1485ebad957857e2bb7adc2ffd1907456
dougday <dougday@105679f1-c027-0410-8e23-b73c1a32fa19>
2007-05-31 20:45:27 +0000

    Updated to version 0.34

--- DDay.iCal.Test/Properties/AssemblyInfo.cs
+++ DDay.iCal.Test/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.32.0.0")]
-[assembly: AssemblyFileVersion("0.32.0.0")]
+[assembly: AssemblyVersion("0.34.0.0")]
+[assembly: AssemblyFileVersion("0.34.0.0")]
