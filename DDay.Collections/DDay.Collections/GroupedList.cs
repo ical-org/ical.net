6018957fa953f646a0c1dadfee183db814b7158f
Rian Stockbower <rstockbower@cimpress.com>
2016-04-07 07:53:01 -0400

    Remove unused references and using statemtents from Collections

--- DDay.Collections/DDay.Collections/GroupedList.cs
+++ DDay.Collections/DDay.Collections/GroupedList.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Linq;
-using System.Runtime.Serialization;
 
 namespace DDay.Collections
 {
--- DDay.Collections/DDay.Collections/Interfaces/IGroupedValueList.cs
+++ DDay.Collections/DDay.Collections/Interfaces/IGroupedValueList.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace DDay.Collections
+﻿namespace DDay.Collections
 {
     public interface IGroupedValueList<TGroup, TInterface, TItem, TValueType> :
         IGroupedValueCollection<TGroup, TInterface, TItem, TValueType>,
