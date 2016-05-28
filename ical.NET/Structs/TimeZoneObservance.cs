7dc62479422f633f61e2ab854ea22c3c9d8e01c0
Rian Stockbower <rstockbower@gmail.com>
2016-05-28 15:35:47 -0400

    More unused code in TimeZoneObservance

--- ical.NET/Structs/TimeZoneObservance.cs
+++ ical.NET/Structs/TimeZoneObservance.cs
@@ -1,17 +1,11 @@
 ﻿using System;
 using Ical.Net.Interfaces.Components;
-using Ical.Net.Interfaces.DataTypes;
 
 namespace Ical.Net.Structs
 {
     [Serializable]
     public struct TimeZoneObservance
     {
-        public ITimeZoneInfo TimeZoneInfo { get; private set; }
-
-        public TimeZoneObservance(IPeriod period, ITimeZoneInfo tzi) : this()
-        {
-            TimeZoneInfo = tzi;
-        }
+        public ITimeZoneInfo TimeZoneInfo { get; }
     }
 }
