6ad1dd0b30fbc8561a37aed5ee186352e1f1752c
Rian Stockbower <rstockbower@gmail.com>
2016-05-28 15:34:00 -0400

    TimeZoneObservance's Period property isn't used by anything

--- ical.NET/Structs/TimeZoneObservance.cs
+++ ical.NET/Structs/TimeZoneObservance.cs
@@ -7,12 +7,10 @@ namespace Ical.Net.Structs
     [Serializable]
     public struct TimeZoneObservance
     {
-        public IPeriod Period { get; private set; }
         public ITimeZoneInfo TimeZoneInfo { get; private set; }
 
         public TimeZoneObservance(IPeriod period, ITimeZoneInfo tzi) : this()
         {
-            Period = period;
             TimeZoneInfo = tzi;
         }
     }
