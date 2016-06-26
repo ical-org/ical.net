cf66e5005f7133ac30cc82e889176f74fea31baf
Rian Stockbower <rstockbower@gmail.com>
2016-06-26 14:40:21 -0400

    Cache MethodInfo per Type lookups. Unit tests completing in 3.6 sec

--- ical.NET/Serialization/iCalendar/Serializers/GenericListSerializer.cs
+++ ical.NET/Serialization/iCalendar/Serializers/GenericListSerializer.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
 using Ical.Net.Interfaces.General;
 using Ical.Net.Interfaces.Serialization;
 using Ical.Net.Interfaces.Serialization.Factory;
@@ -29,6 +30,7 @@ namespace Ical.Net.Serialization.iCalendar.Serializers
             throw new NotImplementedException();
         }
 
+        private MethodInfo _addMethodInfo;
         public override object Deserialize(TextReader tr)
         {
             var p = SerializationContext.Peek() as ICalendarProperty;
@@ -53,29 +55,29 @@ namespace Ical.Net.Serialization.iCalendar.Serializers
                         var objToAdd = stringSerializer.Deserialize(new StringReader(value)) ?? value;
 
                         // FIXME: cache this
-                        var mi = _objectType.GetMethod("Add");
-                        if (mi != null)
+                        if (_addMethodInfo == null)
                         {
-                            // Determine if the returned object is an IList<ObjectType>,
-                            // rather than just an ObjectType.
-                            if (objToAdd is IEnumerable && objToAdd.GetType() == typeof(List<>).MakeGenericType(_innerType))
-                            {
-                                // Deserialization returned an IList<ObjectType>, instead of
-                                // simply an ObjectType.  So, let's enumerate through the
-                                // items in the list and add them individually to our
-                                // list.
-                                foreach (var innerObj in (IEnumerable) objToAdd)
-                                {
-                                    mi.Invoke(listObj, new[] {innerObj});
-                                }
-                            }
-                            else
-                            {
-                                // Add the object to the list
-                                mi.Invoke(listObj, new[] {objToAdd});
-                            }
-                            return listObj;
+                            _addMethodInfo = _objectType.GetMethod("Add");
                         }
+
+                        // Determine if the returned object is an IList<ObjectType>,
+                        // rather than just an ObjectType.
+                        var add = objToAdd as IList;
+                        if (add != null)
+                        {
+                            // Deserialization returned an IList<ObjectType>, instead of an ObjectType.  So enumerate through the items in the list and add
+                            //them individually to our list.
+                            foreach (var innerObj in add)
+                            {
+                                _addMethodInfo.Invoke(listObj, new[] {innerObj});
+                            }
+                        }
+                        else
+                        {
+                            // Add the object to the list
+                            _addMethodInfo.Invoke(listObj, new[] {objToAdd});
+                        }
+                        return listObj;
                     }
                 }
             }
