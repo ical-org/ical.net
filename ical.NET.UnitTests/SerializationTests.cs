f0e288674bf011ea55de7cc2d4dd41c70e9a4c10
Brent McSharry <brentm@adhb.govt.nz>
2016-07-18 19:08:43 +1200

    EnumSerializer converting ToUpper

--- ical.NET.UnitTests/SerializationTests.cs
+++ ical.NET.UnitTests/SerializationTests.cs
@@ -146,9 +146,9 @@ namespace ical.NET.UnitTests
         //This method needs renaming
         static Dictionary<string, string> GetValues(string serialized, string name, string value)
         {
-            var lengthened = serialized.Replace(_newLine + ' ', string.Empty);
+            string coalescedMultiline = Regex.Replace(serialized,_newLine + @"\s" ,string.Empty);
             //using a regex for now - for the sake of speed, it may be worth creating a C# text search later
-            var match = Regex.Match(lengthened, '^' + Regex.Escape(name) + "(;.+)?:" + Regex.Escape(value) + _newLine, RegexOptions.Multiline);
+            var match = Regex.Match(coalescedMultiline, '^' + Regex.Escape(name) + "(;.+)?:" + Regex.Escape(value) + _newLine, RegexOptions.Multiline);
             Assert.IsTrue(match.Success, $"could not find a(n) '{name}' with value '{value}'");
             return match.Groups[1].Value.Length == 0
                 ? new Dictionary<string, string>()
@@ -254,10 +254,9 @@ namespace ical.NET.UnitTests
                 Location = "here",
                 Summary = "test",
                 DtStart = new CalDateTime(2012, 3, 25, 12, 50, 00),
-                DtEnd = new CalDateTime(2012, 3, 25, 13, 10, 00)
-                //not yet testing property below as serialized output currently does not comply with RTFC 2445
-                //Transparency = TransparencyType.Opaque,
-                //Status = EventStatus.Confirmed
+                DtEnd = new CalDateTime(2012, 3, 25, 13, 10, 00),
+                Transparency = TransparencyType.Opaque,
+                Status = EventStatus.Confirmed
             };
             cal.Events.Add(evt);
 
@@ -280,9 +279,9 @@ namespace ical.NET.UnitTests
                 {
                     "CLASS:" + evt.Class, "CREATED:" + CalDateString(evt.Created), "DTSTAMP:" + CalDateString(evt.DtStamp),
                     "LAST-MODIFIED:" + CalDateString(evt.LastModified), "SEQUENCE:" + evt.Sequence, "UID:" + evt.Uid, "PRIORITY:" + evt.Priority,
-                    "LOCATION:" + evt.Location, "SUMMARY:" + evt.Summary, "DTSTART:" + CalDateString(evt.DtStart), "DTEND:" + CalDateString(evt.DtEnd)
-                    //"TRANSPARENCY:" + TransparencyType.Opaque.ToString().ToUpperInvariant(),
-                    //"STATUS:" + EventStatus.Confirmed.ToString().ToUpperInvariant()
+                    "LOCATION:" + evt.Location, "SUMMARY:" + evt.Summary, "DTSTART:" + CalDateString(evt.DtStart), "DTEND:" + CalDateString(evt.DtEnd),
+                    "TRANSP:" + TransparencyType.Opaque.ToString().ToUpperInvariant(),
+                    "STATUS:" + EventStatus.Confirmed.ToString().ToUpperInvariant()
                 });
         }
 
