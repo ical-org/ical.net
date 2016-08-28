08ea67038927062ad5e28dcae16177f49b5a8da3
Rian Stockbower <rstockbower@gmail.com>
2016-08-28 09:10:38 -0400

    Use latest published version in NugetTest project

--- NugetTester/NugetTester/Program.cs
+++ NugetTester/NugetTester/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using Ical.Net;
 using Ical.Net.DataTypes;
@@ -11,14 +12,44 @@ namespace NugetTester
 {
     class Program
     {
+        const string ical = @"BEGIN:VCALENDAR
+BEGIN:VEVENT
+DTSTART;VALUE=DATE:20120823
+DTEND;VALUE=DATE:20120824
+RRULE:FREQ=DAILY;UNTIL=20120824
+EXDATE;VALUE=DATE:20120824
+EXDATE;VALUE=DATE:20120823
+DTSTAMP:20131031T111655Z
+CREATED:20120621T142631Z
+STATUS:CONFIRMED
+SUMMARY:Test Summary
+TRANSP:TRANSPARENT
+END:VEVENT
+END:VCALENDAR";
+
         static void Main(string[] args)
         {
-            var url = new Uri("https://raw.githubusercontent.com/rianjs/ical.net/master/ical.NET.UnitTests/Calendars/Recurrence/Daily1.ics");
-            var webClient = Downloaders.LoadFromUri(url).First();
-            var asyncHttpClient = Downloaders.LoadFromUriAsync(url).Result.First();
-            var syncHttpClient = Downloaders.LoadFromUriSync(url).First();
+            var now = DateTime.Now;
+            var later = now.AddHours(1);
 
-            Console.WriteLine(webClient.Equals(asyncHttpClient) && webClient.Equals(syncHttpClient));
+            var e = new Event
+            {
+                DtStart = new CalDateTime(now),
+                DtEnd = new CalDateTime(later),
+            };
+            e.AddProperty("X-MICROSOFT-CDO-BUSYSTATUS", "OOF"); // I think "OOF" is right per the MS documentation
+
+            var calendar = new Calendar();
+            calendar.Events.Add(e);
+
+            //var serializer = new CalendarSerializer(new SerializationContext());
+            //var icalString = serializer.SerializeToString(calendar);
+            //Console.WriteLine(icalString);
+
+            var collection = Calendar.LoadFromStream(new StringReader(ical));
+            var firstEvent = collection.First().Events.First();
+            var occurrences = firstEvent.GetOccurrences(new CalDateTime(2010, 1, 1, "US-Eastern"), new CalDateTime(2016, 12, 31, "US-Eastern"));
+
             Console.ReadLine();
         }
     }
