cddb49ca8a0fb5e44086f09b82dcc243a4456e2a
Rian Stockbower <rstockbower@gmail.com>
2016-10-18 18:14:16 -0400

    Missing files

---
+++ v2/ical.NET.Collections/Interfaces/IValueObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace ical.net.collections.Interfaces
+{
+    public interface IValueObject<T>
+    {
+        IEnumerable<T> Values { get; }
+
+        bool ContainsValue(T value);
+        void SetValue(T value);
+        void SetValue(IEnumerable<T> values);
+        void AddValue(T value);
+        void RemoveValue(T value);
+        int ValueCount { get; }
+    }
+}
