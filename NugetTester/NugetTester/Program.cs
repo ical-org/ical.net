82510de601ec21edf81806da02b06a8d8a653bc0
Rian Stockbower <rstockbower@cimpress.com>
2017-02-28 13:58:36 -0500

    Update ical.net NugetTester project with some helper methods

--- NugetTester/NugetTester/Program.cs
+++ NugetTester/NugetTester/Program.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Ical.Net;
 using Ical.Net.DataTypes;
-using Ical.Net.Interfaces.DataTypes;
-using Ical.Net.Serialization;
-using Ical.Net.Serialization.iCalendar.Serializers;
 
 namespace NugetTester
 {
@@ -14,27 +10,22 @@ namespace NugetTester
     {
         static void Main(string[] args)
         {
-            var vEvent = new Event
-            {
-                DtStart = new CalDateTime(DateTime.Parse("2016-01-01")),
-                DtEnd = new CalDateTime(DateTime.Parse("2016-01-05")),
-            };
-
-            var vEvent2 = new Event
-            {
-                DtStart = new CalDateTime(DateTime.Parse("2016-03-01")),
-                DtEnd = new CalDateTime(DateTime.Parse("2016-03-05")),
-            };
-
-            var calendar = new Calendar();
-            calendar.Events.Add(vEvent);
-            calendar.Events.Add(vEvent2);
-
-            var searchStart = DateTime.Parse("2015-12-29");
-            var searchEnd = DateTime.Parse("2017-02-10");
-            var occurrences = calendar.GetOccurrences(searchStart, searchEnd);
-
             Console.ReadLine();
+        }
+
+        private static Event DeserializeCalendarEvent(string ical)
+        {
+            var calendar = DeserializeCalendar(ical);
+            var calendarEvent = calendar.First().Events.First() as Event;
+            return calendarEvent;
+        }
+
+        private static CalendarCollection DeserializeCalendar(string ical)
+        {
+            using (var reader = new StringReader(ical))
+            {
+                return Calendar.LoadFromStream(reader) as CalendarCollection;
+            }
         }
     }
 }
