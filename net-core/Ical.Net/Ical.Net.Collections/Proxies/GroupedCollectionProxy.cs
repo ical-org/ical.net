2b025edba8dd9e03a6fec20e8a7be3d11d5293a6
Thomas Wendt <thoemy@gmx.net>
2017-05-15 21:26:49 +0200

    Sync .NET Core Ical.Net.Collections with v2
    The classes were based on the outdated files inside the v2 folder (see
    previous commit). Replace all files with the current classes.

--- net-core/Ical.Net/Ical.Net.Collections/Proxies/GroupedCollectionProxy.cs
+++ net-core/Ical.Net/Ical.Net.Collections/Proxies/GroupedCollectionProxy.cs
@@ -49,7 +49,7 @@ namespace Ical.Net.Collections.Proxies
 
         public virtual bool ContainsKey(TGroup group)
         {
-            return RealObject.ContainsKey(group);
+            return RealObject.ContainsKey(group);            
         }
 
         public virtual int CountOf(TGroup group)
@@ -85,7 +85,10 @@ namespace Ical.Net.Collections.Proxies
             }
         }
 
-        public virtual bool Contains(TNew item) => RealObject.Contains(item);
+        public virtual bool Contains(TNew item)
+        {
+            return RealObject.Contains(item);
+        }
 
         public virtual void CopyTo(TNew[] array, int arrayIndex)
         {
