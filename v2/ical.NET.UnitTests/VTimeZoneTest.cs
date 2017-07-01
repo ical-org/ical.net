0953f69e202a8222e988e41b2a344ddb25791836
beriniwlew <berin@bodyworkbuddy.com>
2017-07-01 13:06:51 -0400

    More timezone tests

--- v2/ical.NET.UnitTests/VTimeZoneTest.cs
+++ v2/ical.NET.UnitTests/VTimeZoneTest.cs
@@ -112,31 +112,7 @@ namespace Ical.Net.UnitTests
             Assert.IsTrue(serialized.Contains("DTSTART:19420209T020000"), "DTSTART:19420209T020000 was not serialized");
             Assert.IsFalse(serialized.Contains("RDATE:19670401/P1D"), "RDate was not properly serialized for vtimezone, should be RDATE:19670401T000000");
         }
-
-        private static Calendar CreateTestCalendar(string tzId)
-        {
-            Calendar iCal = new Calendar();
-           
-            iCal.AddTimeZone(tzId, new DateTime(1900,1,1), true);
-
-            Event calEvent = new Event();
-            calEvent.Description = "Test Recurring Event";
-            calEvent.Start = new CalDateTime(DateTime.Now, tzId);
-            calEvent.End = new CalDateTime(DateTime.Now.AddHours(1), tzId);
-            calEvent.RecurrenceRules = new List<IRecurrencePattern>();
-            calEvent.RecurrenceRules.Add(new RecurrencePattern(FrequencyType.Daily));
-            iCal.Events.Add(calEvent);
-
-            Event calEvent2 = new Event();
-            calEvent2.Description = "Test Recurring Event 2";
-            calEvent2.Start = new CalDateTime(DateTime.Now.AddHours(2), tzId);
-            calEvent2.End = new CalDateTime(DateTime.Now.AddHours(3), tzId);
-            calEvent2.RecurrenceRules = new List<IRecurrencePattern>();
-            calEvent2.RecurrenceRules.Add(new RecurrencePattern(FrequencyType.Daily));
-            iCal.Events.Add(calEvent2);
-            return iCal;
-        }
-
+        
         [Test, Category("VTimeZone")]
         public void VTimeZoneAmericaEirunepeShouldSerializeProperly()
         {
@@ -152,6 +128,9 @@ namespace Ical.Net.UnitTests
             Assert.IsTrue(serialized.Contains("TZNAME:ACST"), "ACST was not serialized");
             Assert.IsTrue(serialized.Contains("TZNAME:ACT"), "ACT was not serialized");
             Assert.IsTrue(serialized.Contains("TZNAME:AMT"), "AMT was not serialized");
+            Assert.IsTrue(serialized.Contains("DTSTART:19311003T110000"), "DTSTART:19311003T110000 was not serialized");
+            Assert.IsTrue(serialized.Contains("DTSTART:19320401T000000"), "DTSTART:19320401T000000 was not serialized");
+            Assert.IsTrue(serialized.Contains("DTSTART:20080624T000000"), "DTSTART:20080624T000000 was not serialized");
 
             // Should not contain the following
             Assert.IsFalse(serialized.Contains("RDATE:19501201T000000/P1D"), "The RDATE was not serialized correctly, should be RDATE:19501201T000000");
@@ -172,6 +151,32 @@ namespace Ical.Net.UnitTests
             Assert.IsTrue(serialized.Contains("TZNAME:EDT"), "EDT was not serialized");
             Assert.IsTrue(serialized.Contains("TZNAME:EPT"), "EPT was not serialized");
             Assert.IsTrue(serialized.Contains("TZNAME:EST"), "EST was not serialized");
+            Assert.IsTrue(serialized.Contains("DTSTART:20070311T020000"), "DTSTART:20070311T020000 was not serialized");
+            Assert.IsTrue(serialized.Contains("DTSTART:20071104T020000"), "DTSTART:20071104T020000 was not serialized");
+        }
+
+        private static Calendar CreateTestCalendar(string tzId)
+        {
+            Calendar iCal = new Calendar();
+
+            iCal.AddTimeZone(tzId, new DateTime(1900, 1, 1), true);
+
+            Event calEvent = new Event();
+            calEvent.Description = "Test Recurring Event";
+            calEvent.Start = new CalDateTime(DateTime.Now, tzId);
+            calEvent.End = new CalDateTime(DateTime.Now.AddHours(1), tzId);
+            calEvent.RecurrenceRules = new List<IRecurrencePattern>();
+            calEvent.RecurrenceRules.Add(new RecurrencePattern(FrequencyType.Daily));
+            iCal.Events.Add(calEvent);
+
+            Event calEvent2 = new Event();
+            calEvent2.Description = "Test Recurring Event 2";
+            calEvent2.Start = new CalDateTime(DateTime.Now.AddHours(2), tzId);
+            calEvent2.End = new CalDateTime(DateTime.Now.AddHours(3), tzId);
+            calEvent2.RecurrenceRules = new List<IRecurrencePattern>();
+            calEvent2.RecurrenceRules.Add(new RecurrencePattern(FrequencyType.Daily));
+            iCal.Events.Add(calEvent2);
+            return iCal;
         }
     }
 }
