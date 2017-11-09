7c1c3ac2968a46395b2ac39b86c002432a4e8841
Rian Stockbower <rstockbower@cimpress.com>
2017-11-09 08:46:53 -0500

    Minor style tweaks to FreeBusySerializer

--- net-core/Ical.Net/Ical.Net/Serialization/iCalendar/Serializers/DataTypes/FreeBusyEntrySerializer.cs
+++ net-core/Ical.Net/Ical.Net/Serialization/iCalendar/Serializers/DataTypes/FreeBusyEntrySerializer.cs
@@ -15,23 +15,25 @@ namespace Ical.Net.Serialization.iCalendar.Serializers.DataTypes
         public override string SerializeToString(object obj)
         {
             var entry = obj as FreeBusyEntry;
-            if (entry != null)
+            if (entry == null)
             {
-                switch (entry.Status)
-                {
-                    case FreeBusyStatus.Busy:
-                        entry.Parameters.Remove("FBTYPE");
-                        break;
-                    case FreeBusyStatus.BusyTentative:
-                        entry.Parameters.Set("FBTYPE", "BUSY-TENTATIVE");
-                        break;
-                    case FreeBusyStatus.BusyUnavailable:
-                        entry.Parameters.Set("FBTYPE", "BUSY-UNAVAILABLE");
-                        break;
-                    case FreeBusyStatus.Free:
-                        entry.Parameters.Set("FBTYPE", "FREE");
-                        break;
-                }
+                return base.SerializeToString(obj);
+            }
+
+            switch (entry.Status)
+            {
+                case FreeBusyStatus.Busy:
+                    entry.Parameters.Remove("FBTYPE");
+                    break;
+                case FreeBusyStatus.BusyTentative:
+                    entry.Parameters.Set("FBTYPE", "BUSY-TENTATIVE");
+                    break;
+                case FreeBusyStatus.BusyUnavailable:
+                    entry.Parameters.Set("FBTYPE", "BUSY-UNAVAILABLE");
+                    break;
+                case FreeBusyStatus.Free:
+                    entry.Parameters.Set("FBTYPE", "FREE");
+                    break;
             }
 
             return base.SerializeToString(obj);
@@ -40,31 +42,36 @@ namespace Ical.Net.Serialization.iCalendar.Serializers.DataTypes
         public override object Deserialize(TextReader tr)
         {
             var entry = base.Deserialize(tr) as FreeBusyEntry;
-            if (entry != null)
+            if (entry == null)
             {
-                if (entry.Parameters.ContainsKey("FBTYPE"))
-                {
-                    var value = entry.Parameters.Get("FBTYPE");
-                    if (value == null)
-                    {
-                        return entry;
-                    }
-                    switch (value.ToUpperInvariant())
-                    {
-                        case "FREE":
-                            entry.Status = FreeBusyStatus.Free;
-                            break;
-                        case "BUSY":
-                            entry.Status = FreeBusyStatus.Busy;
-                            break;
-                        case "BUSY-UNAVAILABLE":
-                            entry.Status = FreeBusyStatus.BusyUnavailable;
-                            break;
-                        case "BUSY-TENTATIVE":
-                            entry.Status = FreeBusyStatus.BusyTentative;
-                            break;
-                    }
-                }
+                return entry;
+            }
+
+            if (!entry.Parameters.ContainsKey("FBTYPE"))
+            {
+                return entry;
+            }
+
+            var value = entry.Parameters.Get("FBTYPE");
+            if (value == null)
+            {
+                return entry;
+            }
+
+            switch (value.ToUpperInvariant())
+            {
+                case "FREE":
+                    entry.Status = FreeBusyStatus.Free;
+                    break;
+                case "BUSY":
+                    entry.Status = FreeBusyStatus.Busy;
+                    break;
+                case "BUSY-UNAVAILABLE":
+                    entry.Status = FreeBusyStatus.BusyUnavailable;
+                    break;
+                case "BUSY-TENTATIVE":
+                    entry.Status = FreeBusyStatus.BusyTentative;
+                    break;
             }
 
             return entry;
