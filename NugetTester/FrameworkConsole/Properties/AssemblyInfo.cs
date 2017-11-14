afc4b4811af4761695b2907841234d30c00ed52a
Rian Stockbower <rstockbower@cimpress.com>
2017-11-14 09:16:50 -0500

    Flesh out nuget tester application a bit #335

--- NugetTester/FrameworkConsole/Properties/AssemblyInfo.cs
+++ NugetTester/FrameworkConsole/Properties/AssemblyInfo.cs
@@ -1,34 +1,34 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NugetTester")]
+[assembly: AssemblyTitle("FrameworkConsole")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NugetTester")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("FrameworkConsole")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ab8a5dba-c2e8-4e6e-85c5-c0c18955cfd1")]
+[assembly: Guid("a2ee82e3-1c20-4d39-97f8-a149520dad79")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
--- NugetTester/NugetTester/FrameworkNuget.cs
+++ NugetTester/NugetTester/FrameworkNuget.cs
@@ -1,16 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using Ical.Net;
+using Ical.Net.CalendarComponents;
 using Ical.Net.DataTypes;
-using Ical.Net.Interfaces.Components;
-using Ical.Net.Interfaces.DataTypes;
-using Ical.Net.Serialization.iCalendar.Serializers;
+using Ical.Net.Serialization;
 
 namespace NugetTester
 {
-    class Program
+    class FrameworkNuget
     {
         static void Main(string[] args)
         {
@@ -21,8 +19,26 @@ namespace NugetTester
                 var serialized = SerializeToString(GetSampleEvent());
                 Console.WriteLine(serialized);
 
-                var unserialized = DeserializeCalendarEvent(serialized);
+                var unserialized = Calendar.Load(serialized).Events.First();
                 Console.WriteLine(unserialized.Start);
+
+                const string brokenIcal = @"BEGIN:VCALENDAR
+PRODID:-//github.com/rianjs/ical.net//NONSGML ical.net 2.0//EN
+VERSION:2.0
+BEGIN:VEVENT
+DESCRIPTION:Foo
+DTEND:20171115T045900Z
+DTSTAMP:20171113T231608Z
+DTSTART:20171114T140000Z
+SEQUENCE:0
+SUMMARY:Blockchain For Wall Street
+UID:7aa68f0e-adc5-4af2-80b9-429ef1f5f193
+END:VEVENT
+END:VCALENDAR";
+
+                var deserializedBroken = Calendar.Load(brokenIcal);
+                var firstEvent = deserializedBroken.Events.First();
+                Console.WriteLine(firstEvent.Start);
             }
             catch (Exception e)
             {
@@ -39,7 +55,7 @@ namespace NugetTester
             {
                 Start = new CalDateTime(_now),
                 End = new CalDateTime(_later),
-                RecurrenceRules = new List<RecurrencePattern> {GetSampleRecurrenceRules()}
+                RecurrenceRules = new List<RecurrencePattern> { GetSampleRecurrenceRules() }
             };
             return e;
         }
@@ -47,23 +63,10 @@ namespace NugetTester
         private static RecurrencePattern GetSampleRecurrenceRules()
             => new RecurrencePattern(FrequencyType.Daily, 1);
 
-        private static CalendarEvent DeserializeCalendarEvent(string ical)
-        {
-            var calendar = DeserializeCalendar(ical);
-            var calendarEvent = calendar.First().Events.First() as CalendarEvent;
-            return calendarEvent;
-        }
+        private static string SerializeToString(CalendarEvent calendarEvent)
+            => SerializeToString(new Calendar { Events = { calendarEvent } });
 
-        private static CalendarCollection DeserializeCalendar(string ical)
-        {
-            using (var reader = new StringReader(ical))
-            {
-                return Calendar.LoadFromStream(reader) as CalendarCollection;
-            }
-        }
-
-        private static string SerializeToString(CalendarEvent calendarEvent) => SerializeToString(new Calendar { Events = { calendarEvent } });
-
-        private static string SerializeToString(Calendar iCalendar) => new CalendarSerializer().SerializeToString(iCalendar);
+        private static string SerializeToString(Calendar iCalendar)
+            => new CalendarSerializer().SerializeToString(iCalendar);
     }
 }
