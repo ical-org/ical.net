0102d9204c654fcbbd5ee361c4adce6461f14824
Rian Stockbower <rstockbower@cimpress.com>
2017-11-22 11:03:44 -0500

    Cache AsUtc value in CalDateTime, because it appears to be a performance bottleneck: after cache, 12-16% improvement in unit test runtime
    Unscientific analysis:
    
    |        | Framework | Core    |
    |--------|-----------|---------|
    | Before | 2.388     | 1.787   |
    |        | 2.174     | 1.592   |
    |        | 2.189     | 1.599   |
    |        | *2.250*   | *1.659* |
    | After  | 1.954     | 1.365   |
    |        | 2.008     | 1.409   |
    |        | 1.961     | 1.382   |
    |        | *1.974*    | *1.385* |
    |        | **-0.276 sec (12.265%)** | **-0.274 sec (16.513%)**

--- net-core/Ical.Net.CoreUnitTests/ComponentTest.cs
+++ net-core/Ical.Net.CoreUnitTests/ComponentTest.cs
@@ -1,12 +1,13 @@
+using System;
 using Ical.Net.CalendarComponents;
+using Ical.Net.DataTypes;
 using NUnit.Framework;
 
 namespace Ical.Net.CoreUnitTests
 {
-    [TestFixture]
     public class ComponentTest
     {
-        [Test, Category("Component")]
+        [Test, Category("Components")]
         public void UniqueComponent1()
         {
             var iCal = new Calendar();
@@ -15,6 +16,28 @@ namespace Ical.Net.CoreUnitTests
             Assert.IsNotNull(evt.Uid);
             Assert.IsNull(evt.Created); // We don't want this to be set automatically
             Assert.IsNotNull(evt.DtStamp);
+        }
+
+        [Test, Category("Components")]
+        public void ChangeCalDateTimeValue()
+        {
+            var e = new CalendarEvent
+            {
+                Start = new CalDateTime(2017, 11, 22, 11, 00, 01),
+                End = new CalDateTime(2017, 11, 22, 11, 30, 01),
+            };
+
+            var firstStartAsUtc = e.Start.AsUtc;
+            var firstEndAsUtc = e.End.AsUtc;
+
+            e.Start.Value = new DateTime(2017, 11, 22, 11, 30, 01);
+            e.End.Value = new DateTime(2017, 11, 22, 12, 00, 01);
+
+            var secondStartAsUtc = e.Start.AsUtc;
+            var secondEndAsUtc = e.End.AsUtc;
+
+            Assert.AreNotEqual(firstStartAsUtc, secondStartAsUtc);
+            Assert.AreNotEqual(firstEndAsUtc, secondEndAsUtc);
         }
     }
 }
