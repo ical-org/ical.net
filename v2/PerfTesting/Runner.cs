142010b606b9dfedae7843fcf5b9785c78790072
Rian Stockbower <rstockbower@gmail.com>
2017-11-24 11:56:30 -0500

    Compare simple deserializer to ANTLR deserializer -- simple is about twice as fast

---
+++ v2/PerfTesting/Runner.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Toolchains.InProcess;
+
+namespace PerfTesting
+{
+    class Runner
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunnerCore.Run(BenchmarkConverter.TypeToBenchmarks(typeof(SerializationPerfTests)), t => InProcessToolchain.Instance);
+        }
+    }
+}
