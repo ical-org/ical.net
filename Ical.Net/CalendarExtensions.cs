f7697a364ae6fc2cc9b7bf8b7e488706850bd778
Markus Minichmayr <markus@tapkey.com>
2024-11-26 21:22:11 +0100

    Fix BYWEEKNO recurrence evaluation; consider negative _BY_ values.

--- Ical.Net/CalendarExtensions.cs
+++ Ical.Net/CalendarExtensions.cs
@@ -39,4 +39,34 @@ public static class CalendarExtensions
         var tn = ((int) t.DayOfWeek) % 7;
         return t.AddDays(-((tn + 7 - t0) % 7));
     }
+
+    /// <summary>
+    /// Calculate the year, the given date's week belongs to according to ISO 8601, as required by RFC 5545.
+    /// </summary>
+    /// <remarks>
+    /// A date's nominal year may be different from the year, the week belongs to that the date is in.
+    /// I.e. the first and last week of the year may belong to a different year than the date's year.
+    /// E.g. for `2019-12-31` with first day of the week being Monday, the method will return 2020,
+    /// because the week that contains `2019-12-31` is the first week of 2020.
+    /// </remarks>
+    public static int GetIso8601YearOfWeek(this System.Globalization.Calendar calendar, DateTime time, DayOfWeek firstDayOfWeek)
+    {
+        var year = time.Year;
+        if ((time.Month >= 12) && (calendar.GetIso8601WeekOfYear(time, firstDayOfWeek) == 1))
+            year++;
+        else if ((time.Month == 1) && (calendar.GetIso8601WeekOfYear(time, firstDayOfWeek) >= 52))
+            year--;
+
+        return year;
+    }
+
+    /// <summary>
+    /// Calculate the number of weeks in the given year according to ISO 8601, as required by RFC 5545.
+    /// </summary>
+    public static int GetIso8601WeeksInYear(this System.Globalization.Calendar calendar, int year, DayOfWeek firstDayOfWeek)
+    {
+        // The last week of the year is the week that contains the 4th-last day of the year (which is the 28th of December in Gregorian Calendar).
+        var testTime = new DateTime(year + 1, 1, 1, 0, 0, 0, DateTimeKind.Unspecified).AddDays(-4);
+        return calendar.GetIso8601WeekOfYear(testTime, firstDayOfWeek);
+    }
 }
