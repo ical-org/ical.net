3e5dea189f397089ff3ddf838e39eac8689054ac
Markus Minichmayr <markus@tapkey.com>
2024-11-29 08:18:54 +0100

    CollectionHelpers: Introduce streaming operators on ordered `IEnumerable`s, `OrderedMerge`, `OrderedMergeMany`, `OrderedDistinct`.

--- Ical.Net.Tests/CollectionHelpersTests.cs
+++ Ical.Net.Tests/CollectionHelpersTests.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Ical.Net.DataTypes;
+using Ical.Net.Utility;
 using NUnit.Framework;
 
 namespace Ical.Net.Tests;
@@ -14,11 +15,7 @@ namespace Ical.Net.Tests;
 internal class CollectionHelpersTests
 {
     private static readonly DateTime _now = DateTime.UtcNow;
-    private static readonly DateTime _later = _now.AddHours(1);
-    private static readonly string _uid = Guid.NewGuid().ToString();
 
-    private static List<RecurrencePattern> GetSimpleRecurrenceList()
-        => new List<RecurrencePattern> { new RecurrencePattern(FrequencyType.Daily, 1) { Count = 5 } };
     private static List<PeriodList> GetExceptionDates()
         => new List<PeriodList> { new PeriodList { new Period(new CalDateTime(_now.AddDays(1).Date)) } };
 
@@ -36,5 +33,66 @@ internal class CollectionHelpersTests
         changedPeriod.First().First().StartTime = new CalDateTime(_now.AddHours(-1));
 
         Assert.That(changedPeriod, Is.Not.EqualTo(GetExceptionDates()));
+    }
+
+    [TestCase(new[] { 1, 3, 5, 7 }, new[] { 2, 4, 6 }, new[] { 1, 2, 3, 4, 5, 6, 7 })]
+    [TestCase(new int[] { }, new int[] { }, new int[] { })]
+    [TestCase(new int[] { }, new[] { 2, 4, 6 }, new[] { 2, 4, 6 })]
+    [TestCase(new[] { 2, 4, 6 }, new int[] { }, new[] { 2, 4, 6 })]
+    [TestCase(new[] { 3, 4 }, new int[] { 1, 2 }, new[] { 1, 2, 3, 4 })]
+    [TestCase(new[] { 1, 2, 3 }, new int[] { 2, 3, 4 }, new[] { 1, 2, 2, 3, 3, 4 })]
+    public void TestMerge(IList<int> seq1, IList<int> seq2, IList<int> expected)
+    {
+        var result = seq1.OrderedMerge(seq2).ToList();
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [TestCase(new int[] { }, new int[] { }, new int[] { })]
+    [TestCase(new int[] { }, new[] { 2, 4, 6 }, new int[] { })]
+    [TestCase(new[] { 2, 4, 6 }, new int[] { }, new[] { 2, 4, 6 })]
+    [TestCase(new[] { 1, 2, 3, 5, 6, 7 }, new[] { 2, 4, 6 }, new[] { 1, 3, 5, 7 })]
+    public void TestMergeExclude(IList<int> seq, IList<int> exclude, IList<int> expected)
+    {
+        var result = seq.OrderedExclude(exclude).ToList();
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    private static IEnumerable<int> GetNaturalNumbers()
+    {
+        var i = 1;
+        while (true)
+            yield return i++;
+    }
+
+    [Test]
+    public void TestMergeIndefinite()
+    {
+        var result = GetNaturalNumbers().Select(x => x * 3).OrderedMerge(GetNaturalNumbers().Select(x => x * 2))
+            .Take(7);
+        Assert.That(result, Is.EqualTo(new[] { 2, 3, 4, 6, 6, 8, 9 }));
+    }
+
+    [Test]
+    public void TestMergeExcludeIndefinite()
+    {
+        var result = GetNaturalNumbers().Select(x => x * 3).OrderedExclude(GetNaturalNumbers().Select(x => x * 2))
+            .Take(4);
+        Assert.That(result, Is.EqualTo(new[] { 3, 9, 15, 21 }));
+    }
+
+    [Test]
+    public void TestMergeMulti()
+    {
+        var result = CollectionHelpers.OrderedMergeMany([[4], [2], [3], [1]], Comparer<int>.Default);
+        Assert.That(result, Is.EqualTo(new[] { 1, 2, 3, 4 }));
+    }
+
+    [Test]
+    public void TestOrderedDistinct()
+    {
+        var result = new[] { 1, 2, 2, 3 }.OrderedDistinct();
+        Assert.That(result, Is.EqualTo(new[] { 1, 2, 3 }));
     }
 }
--- Ical.Net/Utility/CollectionHelpers.cs
+++ Ical.Net/Utility/CollectionHelpers.cs
@@ -89,4 +89,184 @@ internal static class CollectionHelpers
             destination.Add(element);
         }
     }
+
+    /// <summary>
+    /// Merge the given ordered sequences into a single ordered sequence.
+    /// </summary>
+    /// <remarks>
+    /// Each input sequence must be ordered according to the given comparer. Duplicates are allowed and will be preserved.
+    /// 
+    /// The method operates in a streaming manner, meaning it only enumerates the input sequences while the
+    /// output sequence is being enumerated, and can therefore handle indefinite sequences.
+    /// </remarks>
+    public static IEnumerable<T> OrderedMerge<T>(this IEnumerable<T> items, IEnumerable<T> other)
+        => items.OrderedMerge(other, Comparer<T>.Default);
+
+    /// <summary>
+    /// Merge the given ordered sequences into a single ordered sequence.
+    /// </summary>
+    /// <remarks>
+    /// Each input sequence must be ordered according to the given comparer. Duplicates are allowed and will be preserved.
+    /// 
+    /// The method operates in a streaming manner, meaning it only enumerates the input sequences while the
+    /// output sequence is being enumerated, and can therefore handle indefinite sequences.
+    /// </remarks>
+    public static IEnumerable<T> OrderedMerge<T>(this IEnumerable<T> items, IEnumerable<T> other, IComparer<T> comparer)
+    {
+        using var it1 = items.GetEnumerator();
+        using var it2 = other.GetEnumerator();
+
+        var has1 = it1.MoveNext();
+        var has2 = it2.MoveNext();
+
+        while (has1 || has2)
+        {
+            var cmp = (has1, has2) switch
+            {
+                (true, false) => -1,
+                (false, true) => 1,
+                _ => comparer.Compare(it1.Current, it2.Current)
+            };
+
+            if (cmp <= 0)
+            {
+                yield return it1.Current;
+                has1 = it1.MoveNext();
+            }
+            else
+            {
+                yield return it2.Current;
+                has2 = it2.MoveNext();
+            }
+        }
+    }
+
+    /// <summary>
+    /// Merge the given ordered sequences into a single ordered sequence.
+    /// </summary>
+    /// <remarks>
+    /// Each input sequence must be ordered according to types default comparer. Duplicates are allowed and will be preserved.
+    /// 
+    /// The method operates in a streaming manner, meaning it only enumerates the input sequences while the
+    /// output sequence is being enumerated, and can therefore handle indefinite sequences.
+    /// </remarks>
+    public static IEnumerable<T> OrderedMergeMany<T>(this IEnumerable<IEnumerable<T>> sequences)
+        => OrderedMergeMany(sequences, Comparer<T>.Default);
+
+    /// <summary>
+    /// Merge the given ordered sequences into a single ordered sequence.
+    /// </summary>
+    /// <remarks>
+    /// Each input sequence must be ordered according to the given comparer. Duplicates are allowed and will be preserved.
+    /// 
+    /// The method operates in a streaming manner, meaning it only enumerates the input sequences while the
+    /// output sequence is being enumerated, and can therefore handle indefinite sequences.
+    /// </remarks>
+    public static IEnumerable<T> OrderedMergeMany<T>(this IEnumerable<IEnumerable<T>> sequences, IComparer<T> comparer)
+    {
+        var list = (sequences as IList<IEnumerable<T>>) ?? sequences.ToList();
+        return OrderedMergeMany(list, 0, list.Count, comparer);
+    }
+
+    private static IEnumerable<T> OrderedMergeMany<T>(this IList<IEnumerable<T>> sequences, int offs, int length, IComparer<T> comparer)
+    {
+        if (length == 0)
+            return [];
+
+        if (length == 1)
+            return sequences[offs];
+
+        // Compose as a tree to ensure O(N*log(N)) complexity. Composing as a simple chain
+        // would result in O(N*N) complexity, which wouldn't be a problem either, as
+        // the number of sequences usually is low.
+        var mid = length / 2;
+        var left = OrderedMergeMany(sequences, offs, mid, comparer);
+        var right = OrderedMergeMany(sequences, offs + mid, length - mid, comparer);
+
+        return left.OrderedMerge(right, comparer);
+    }
+
+    /// <summary>
+    /// Returns the elements of the first ordered sequence that are not present in the second ordered sequence.
+    /// </summary>
+    /// <remarks>
+    /// Both sequences must be ordered according to the type's default comparer.
+    ///
+    /// The method operates in a streaming manner, meaning it only enumerates the input sequences while the
+    /// output sequence is being enumerated, and can therefore handle indefinite sequences.
+    /// </remarks>
+    public static IEnumerable<T> OrderedExclude<T>(this IEnumerable<T> items, IEnumerable<T> exclude)
+        => items.OrderedExclude(exclude, Comparer<T>.Default);
+
+    /// <summary>
+    /// Returns the elements of the first ordered sequence that are not present in the second ordered sequence.
+    /// </summary>
+    /// <remarks>
+    /// Both sequences must be ordered according to the specified comparer.
+    ///
+    /// The method operates in a streaming manner, meaning it only enumerates the input sequences while the
+    /// output sequence is being enumerated, and can therefore handle indefinite sequences.
+    /// </remarks>
+    public static IEnumerable<T> OrderedExclude<T>(this IEnumerable<T> items, IEnumerable<T> exclude, IComparer<T> comparer)
+    {
+        using var it = items.GetEnumerator();
+        using var itEx = exclude.GetEnumerator();
+
+        var hasNextIt = it.MoveNext();
+        var hasNextEx = itEx.MoveNext();
+
+        while (hasNextIt)
+        {
+            var cmp = hasNextEx ? comparer.Compare(it.Current, itEx.Current) : -1;
+            if (cmp <= 0)
+            {
+                if (cmp < 0)
+                    yield return it.Current;
+
+                hasNextIt = it.MoveNext();
+            }
+            else
+            {
+                hasNextEx = itEx.MoveNext();
+            }
+        }
+    }
+
+    /// <summary>
+    /// Returns a sequence containing the items of the ordered input sequence, with duplicates removed.
+    /// </summary>
+    /// <remarks>
+    /// The input sequence must be ordered according to the type's default equality comparer, such
+    /// that equal items are adjacent to each other.
+    ///
+    /// The method operates in a streaming manner, meaning it only enumerates the input sequence while the
+    /// output sequence is enumerated and can therefore handle indefinite sequences.
+    /// </remarks>
+    public static IEnumerable<T> OrderedDistinct<T>(this IEnumerable<T> items)
+        => items.OrderedDistinct(EqualityComparer<T>.Default);
+
+    /// <summary>
+    /// Returns a sequence containing the items of the ordered input sequence, with duplicates removed.
+    /// </summary>
+    /// <remarks>
+    /// The input sequence must be ordered according to the type's default equality comparer, such
+    /// that equal items are adjacent to each other.
+    ///
+    /// The method operates in a streaming manner, meaning it only enumerates the input sequence while the
+    /// output sequence is enumerated and can therefore handle indefinite sequences.
+    /// </remarks>
+    public static IEnumerable<T> OrderedDistinct<T>(this IEnumerable<T> items, IEqualityComparer<T> comparer)
+    {
+        var prev = default(T);
+        var first = true;
+
+        foreach (var item in items)
+        {
+            if (first || !comparer.Equals(prev, item))
+                yield return item;
+
+            prev = item;
+            first = false;
+        }
+    }
 }
