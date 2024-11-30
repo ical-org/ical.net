3fcbbcd76b5677cebf98045901b3e5e38f0f1c29
Markus Minichmayr <markus@tapkey.com>
2024-11-30 20:33:03 +0100

    IEvaluator, RecurrenceUtil: Default `includeReferenceDateInResults` to `true` as `DTSTART` is always included according to RFC 5545. However, in the future it would be desirable to have finer control, i.e. if the RRULE doesn't match DTSTART, which is undefined by the RFC. In this case it would most often be better *not* to include DTSTART.

--- Ical.Net/Evaluation/RecurrenceUtil.cs
+++ Ical.Net/Evaluation/RecurrenceUtil.cs
@@ -14,10 +14,10 @@ namespace Ical.Net.Evaluation;
 
 internal class RecurrenceUtil
 {
-    public static IEnumerable<Occurrence> GetOccurrences(IRecurrable recurrable, IDateTime dt, bool includeReferenceDateInResults) => GetOccurrences(recurrable,
+    public static IEnumerable<Occurrence> GetOccurrences(IRecurrable recurrable, IDateTime dt, bool includeReferenceDateInResults = true) => GetOccurrences(recurrable,
         new CalDateTime(dt.Date), new CalDateTime(dt.Date.AddDays(1)), includeReferenceDateInResults);
 
-    public static IEnumerable<Occurrence> GetOccurrences(IRecurrable recurrable, IDateTime periodStart, IDateTime periodEnd, bool includeReferenceDateInResults)
+    public static IEnumerable<Occurrence> GetOccurrences(IRecurrable recurrable, IDateTime periodStart, IDateTime periodEnd, bool includeReferenceDateInResults = true)
     {
         var evaluator = recurrable.GetService(typeof(IEvaluator)) as IEvaluator;
         if (evaluator == null || recurrable.Start == null)
