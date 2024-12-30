0d68f17623f3a2948ae162b90c3e7c86bbf7cd2a
axunonb <axunonb@users.noreply.github.com>
2024-12-30 11:12:39 +0100

    Add unit tests to `RecurrenceWithRDateTests`

--- Ical.Net.Tests/RecurrenceWithRDateTests.cs
+++ Ical.Net.Tests/RecurrenceWithRDateTests.cs
@@ -139,4 +139,220 @@ public class RecurrenceWithRDateTests
             Assert.That(occurrences[2].Period.EffectiveDuration, Is.EqualTo(new Duration(hours: 5)));
         });
     }
+
+    [Test]
+    public void RDate_MixedDatesAndPeriods_AreProcessedCorrectly()
+    {
+        var cal = new Calendar();
+        var eventStart = new CalDateTime(2023, 10, 1, 10, 0, 0);
+        var recurrenceDates = new PeriodList
+        {
+            new Period(new CalDateTime(2023, 10, 2, 10, 0, 0)),
+            new Period(new CalDateTime(2023, 10, 3, 10, 0, 0), new Duration(hours: 3))
+        };
+
+        var calendarEvent = new CalendarEvent
+        {
+            Start = eventStart,
+            Duration = new Duration(hours: 1),
+            RecurrenceDates = new List<PeriodList> { recurrenceDates }
+        };
+
+        cal.Events.Add(calendarEvent);
+        var serializer = new CalendarSerializer();
+        var ics = serializer.SerializeToString(cal);
+
+        var occurrences = calendarEvent.GetOccurrences().ToList();
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(occurrences, Has.Count.EqualTo(3));
+            Assert.That(occurrences[0].Period.StartTime, Is.EqualTo(new CalDateTime(2023, 10, 1, 10, 0, 0)));
+            Assert.That(occurrences[1].Period.StartTime, Is.EqualTo(new CalDateTime(2023, 10, 2, 10, 0, 0)));
+            Assert.That(occurrences[2].Period.StartTime, Is.EqualTo(new CalDateTime(2023, 10, 3, 10, 0, 0)));
+            Assert.That(occurrences[2].Period.EffectiveDuration, Is.EqualTo(new Duration(hours: 3)));
+            Assert.That(ics, Does.Contain("RDATE:20231002T100000,20231003T100000/PT3H"));
+        });
+    }
+
+    [Test]
+    public void RDate_DifferentTimeZones_AreProcessedCorrectly()
+    {
+        var cal = new Calendar();
+        var eventStart = new CalDateTime(2023, 10, 1, 10, 0, 0, "America/New_York");
+        var recurrenceDates1 = new PeriodList
+        {
+            new Period(new CalDateTime(2023, 10, 2, 10, 0, 0, "America/Los_Angeles"))
+        };
+        var recurrenceDates2 = new PeriodList
+        {
+            new Period(new CalDateTime(2023, 10, 3, 10, 0, 0, "Europe/London"))
+        };
+
+        var calendarEvent = new CalendarEvent
+        {
+            Start = eventStart,
+            Duration = new Duration(hours: 1),
+            RecurrenceDates = new List<PeriodList> { recurrenceDates1, recurrenceDates2 }
+        };
+
+        cal.Events.Add(calendarEvent);
+        var serializer = new CalendarSerializer();
+        var ics = serializer.SerializeToString(cal);
+
+        var occurrences = calendarEvent.GetOccurrences().ToList();
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(occurrences, Has.Count.EqualTo(3));
+            Assert.That(occurrences[0].Period.StartTime,
+                Is.EqualTo(new CalDateTime(2023, 10, 1, 10, 0, 0, "America/New_York")));
+            Assert.That(occurrences[1].Period.StartTime,
+                Is.EqualTo(new CalDateTime(2023, 10, 2, 10, 0, 0, "America/Los_Angeles")));
+            Assert.That(occurrences[2].Period.StartTime,
+                Is.EqualTo(new CalDateTime(2023, 10, 3, 10, 0, 0, "Europe/London")));
+            Assert.That(ics, Does.Contain("RDATE;TZID=America/Los_Angeles:20231002T100000"));
+            Assert.That(ics, Does.Contain("RDATE;TZID=Europe/London:20231003T100000"));
+        });
+    }
+
+    [Test]
+    public void AddingDifferentTimeZonesToPeriodList_ShouldThrow()
+    {
+        Assert.That(() =>
+        {
+            _ = new PeriodList
+            {
+                new Period(new CalDateTime(2023, 10, 2, 10, 0, 0, "America/Los_Angeles")),
+                new Period(new CalDateTime(2023, 10, 3, 10, 0, 0, "Europe/London"))
+            };
+        }, Throws.ArgumentException);
+    }
+
+    [Test]
+    public void RDate_DateOnlyAndDateTime_AreProcessedCorrectly()
+    {
+        var cal = new Calendar();
+        var eventStart = new CalDateTime(2023, 10, 1, 10, 0, 0);
+        var recurrenceDates = new PeriodList
+        {
+            new Period(new CalDateTime(2023, 10, 2), new Duration(days: 1)),
+            new Period(new CalDateTime(2023, 10, 3, 10, 0, 0))
+        };
+
+        var calendarEvent = new CalendarEvent
+        {
+            Start = eventStart,
+            Duration = new Duration(days: 1),
+            RecurrenceDates = new List<PeriodList> { recurrenceDates }
+        };
+
+        cal.Events.Add(calendarEvent);
+        var serializer = new CalendarSerializer();
+        var ics = serializer.SerializeToString(cal);
+
+        var occurrences = calendarEvent.GetOccurrences().ToList();
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(occurrences, Has.Count.EqualTo(3));
+            Assert.That(occurrences[0].Period.StartTime, Is.EqualTo(new CalDateTime(2023, 10, 1, 10, 0, 0)));
+            Assert.That(occurrences[1].Period.StartTime, Is.EqualTo(new CalDateTime(2023, 10, 2)));
+            Assert.That(occurrences[1].Period.EffectiveDuration, Is.EqualTo(new Duration(days: 1)));
+            Assert.That(occurrences[2].Period.StartTime, Is.EqualTo(new CalDateTime(2023, 10, 3, 10, 0, 0)));
+            Assert.That(ics, Does.Contain("RDATE:20231002/P1D,20231003T100000"));
+            Assert.That(ics, Does.Contain("DURATION:P1D"));
+        });
+    }
+
+    [Test]
+    public void RDate_OverlappingPeriods_AreProcessedCorrectly()
+    {
+        var cal = new Calendar();
+        var eventStart = new CalDateTime(2023, 10, 1, 10, 0, 0);
+        var recurrenceDates = new PeriodList
+        {
+            new Period(new CalDateTime(2023, 10, 2, 10, 0, 0), new Duration(hours: 2)),
+            new Period(new CalDateTime(2023, 10, 2, 11, 0, 0), new Duration(hours: 2))
+        };
+
+        var calendarEvent = new CalendarEvent
+        {
+            Start = eventStart,
+            Duration = new Duration(hours: 1),
+            RecurrenceDates = new List<PeriodList> { recurrenceDates }
+        };
+
+        cal.Events.Add(calendarEvent);
+        var serializer = new CalendarSerializer();
+        var ics = serializer.SerializeToString(cal);
+
+        var occurrences = calendarEvent.GetOccurrences().ToList();
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(occurrences, Has.Count.EqualTo(3));
+            Assert.That(occurrences[0].Period.StartTime, Is.EqualTo(new CalDateTime(2023, 10, 1, 10, 0, 0)));
+            Assert.That(occurrences[1].Period.StartTime, Is.EqualTo(new CalDateTime(2023, 10, 2, 10, 0, 0)));
+            Assert.That(occurrences[2].Period.StartTime, Is.EqualTo(new CalDateTime(2023, 10, 2, 11, 0, 0)));
+            Assert.That(ics, Does.Contain("RDATE:20231002T100000/PT2H,20231002T110000/PT2H"));
+        });
+    }
+
+    [Test]
+    public void RDate_DateOnly_WithExactDuration_ShouldThrow()
+    {
+        var eventStart = new CalDateTime(2023, 10, 1, 10, 0, 0, "America/New_York");
+        var recurrenceDates = new PeriodList
+        {
+            new Period(new CalDateTime(2023, 10, 2)),
+        };
+
+        var calendarEvent = new CalendarEvent
+        {
+            Start = eventStart,
+            Duration = new Duration(hours: 1), // Exact duration cannot be added to date-only recurrence
+            RecurrenceDates = new List<PeriodList> { recurrenceDates }
+        };
+
+        Assert.That(() => { _ = calendarEvent.GetOccurrences().ToList(); }, Throws.InvalidOperationException);
+    }
+
+    [Test]
+    public void RDate_LargeNumberOfDates_ShouldBeLineFolded()
+    {
+        var cal = new Calendar();
+        var eventStart = new CalDateTime(2023, 10, 1, 10, 0, 0);
+        var recurrenceDates = new PeriodList();
+        for (var i = 1; i <= 100; i++) // Adjusted to create 100 dates
+        {
+            recurrenceDates.Add(new Period(eventStart.AddDays(i)));
+        }
+
+        var calendarEvent = new CalendarEvent
+        {
+            Start = eventStart,
+            Duration = new Duration(hours: 1),
+            RecurrenceDates = new List<PeriodList> { recurrenceDates }
+        };
+
+        cal.Events.Add(calendarEvent);
+        var serializer = new CalendarSerializer();
+        var ics = serializer.SerializeToString(cal);
+
+        var occurrences = calendarEvent.GetOccurrences().ToList();
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(occurrences, Has.Count.EqualTo(101)); // Including the original event
+            for (var i = 0; i < 101; i++)
+            {
+                Assert.That(occurrences[i].Period.StartTime, Is.EqualTo(eventStart.AddDays(i)));
+            }
+            // First folded line is 75 characters long
+            Assert.That(ics, Does.Contain("RDATE:20231002T100000,20231003T100000,20231004T100000,20231005T100000,2023"));
+            // Last folded line
+            Assert.That(ics, Does.Contain(" T100000,20240106T100000,20240107T100000,20240108T100000,20240109T100000"));
+        });
+    }
 }
