366b7056a9276167be0105e537d7bc9ff0c52c88
axunonb <axunonb@users.noreply.github.com>
2025-01-20 22:50:41 +0100

    Remove obsolete recurrence restriction and evaluation types (#699)
    The changes remove the usage of `RecurrenceRestrictionType` and
    `RecurrenceEvaluationModeType` from various parts of the codebase.
    These types and their associated properties and methods were marked
    as obsolete and have now been completely removed.
    
    - In `RecurrenceTests.cs`, the code that set the `RestrictionType`
      property on recurrence rules has been removed.
    - In `Calendar.cs`, the obsolete properties `RecurrenceRestriction`
      and `RecurrenceEvaluationMode` have been removed.
    - In `Constants.cs`, the obsolete enums `RecurrenceRestrictionType`
      and `RecurrenceEvaluationModeType` have been removed.
    - In `RecurrencePattern.cs`, the properties `RestrictionType` and
      `EvaluationMode` have been removed, along with their associated logic.
    - In `RecurrencePatternEvaluator.cs`, the method `EnforceEvaluationRestrictions`
      and its calls have been removed.
    
    The changes also include some adjustments to ensure that the removal
    of these properties and methods does not affect the overall functionality
    of the code.

--- Ical.Net/Constants.cs
+++ Ical.Net/Constants.cs
@@ -223,58 +223,6 @@ public enum FrequencyOccurrence
     Fifth = 5
 }
 
-[Obsolete("Usage may cause undesired results or exceptions. Will be removed.", false)]
-public enum RecurrenceRestrictionType
-{
-    /// <summary>
-    /// Same as RestrictSecondly.
-    /// </summary>
-    Default,
-
-    /// <summary>
-    /// Does not restrict recurrence evaluation - WARNING: this may cause very slow performance!
-    /// </summary>
-    NoRestriction,
-
-    /// <summary>
-    /// Disallows use of the SECONDLY frequency for recurrence evaluation
-    /// </summary>
-    RestrictSecondly,
-
-    /// <summary>
-    /// Disallows use of the MINUTELY and SECONDLY frequencies for recurrence evaluation
-    /// </summary>
-    RestrictMinutely,
-
-    /// <summary>
-    /// Disallows use of the HOURLY, MINUTELY, and SECONDLY frequencies for recurrence evaluation
-    /// </summary>
-    RestrictHourly
-}
-
-[Obsolete("Usage may cause undesired results or exceptions. Will be removed.", false)]
-public enum RecurrenceEvaluationModeType
-{
-    /// <summary>
-    /// Same as ThrowException.
-    /// </summary>
-    Default,
-
-    /// <summary>
-    /// Automatically adjusts the evaluation to the next-best frequency based on the restriction type.
-    /// For example, if the restriction were IgnoreSeconds, and the frequency were SECONDLY, then
-    /// this would cause the frequency to be adjusted to MINUTELY, the next closest thing.
-    /// </summary>
-    AdjustAutomatically,
-
-    /// <summary>
-    /// This will throw an exception if a recurrence rule is evaluated that does not meet the minimum
-    /// restrictions.  For example, if the restriction were IgnoreSeconds, and a SECONDLY frequency
-    /// were evaluated, an exception would be thrown.
-    /// </summary>
-    ThrowException
-}
-
 public static class TransparencyType
 {
     public const string Name = "TRANSP";
