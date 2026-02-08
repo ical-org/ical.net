9b45e71dcb662f8fd2d31ccdf58250d3f312a991
Markus Minichmayr <markus@tapkey.com>
2026-02-08 20:23:36 +0100

    Test: Apply `START-AT` to expected recurrence instances in file-based recurrence tests as required by libical tests

--- Ical.Net.Tests/RecurrenceTests.cs
+++ Ical.Net.Tests/RecurrenceTests.cs
@@ -3490,9 +3490,12 @@ END:VCALENDAR";
         }
 
         var occurrences = EnumerateOccurrences();
+        var expectedInstances = testCase.Instances;
+        if (testCase.StartAt != null)
+            expectedInstances = expectedInstances?.Where(x => x.Value >= testCase.StartAt.Value).ToList();
 
         var startDates = occurrences.Select(x => x.Start).ToList();
-        Assert.That(startDates, Is.EqualTo(testCase.Instances?.Select(x => x.ToZonedDateTime(timeZone)).ToList()));
+        Assert.That(startDates, Is.EqualTo(expectedInstances?.Select(x => x.ToZonedDateTime(timeZone)).ToList()));
     }
 
     [Test]
