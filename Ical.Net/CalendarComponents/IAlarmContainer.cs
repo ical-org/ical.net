5462d6fb06a358e2b49a3900960085d5831d5f2f
axunonb <axunonb@users.noreply.github.com>
2026-04-16 13:09:51 +0200

    feat(alarms)!: Major overhaul of alarm evaluation and RFC 5545 compliance
    Changes:
    - Introduce GetAlarmOccurrences for streaming, ordered alarm fire times with optional time bounds, replacing PollAlarms/Poll.
    - Refactor Alarm to use GetFireTimes for RFC 5545-compliant repetition logic.
    - Merge and order alarm occurrences across all alarms, handling RELATED=END and RDATE periods correctly.
    - Add OrderedNestedMergeMany for efficient streaming merge of ordered sequences.
    - Make AlarmOccurrence comparable by fire time.
    - Add more AlarmTests
    
    BREAKING CHANGE: IAlarmContainer.PollAlarms(DateTimeZone, Instant?, Instant?) has been
    removed. Replace calls with GetAlarmOccurrences(timeZone, startTime, endTime, options).
    The new method returns IEnumerable<AlarmOccurrence> ordered by fire time, accepts an
    optional EvaluationOptions argument, and correctly handles RELATED=END triggers on
    PERIOD RDATEs with varying durations.
    
    Credits to @minichma for providing the code for streaming Alarm occurrences (https://github.com/minichma/ical.net/tree/xp/alarm_streaming)

--- Ical.Net/CalendarComponents/IAlarmContainer.cs
+++ Ical.Net/CalendarComponents/IAlarmContainer.cs
@@ -1,10 +1,11 @@
-﻿//
+//
 // Copyright ical.net project maintainers and contributors.
 // Licensed under the MIT license.
 //
 
 using System.Collections.Generic;
 using Ical.Net.DataTypes;
+using Ical.Net.Evaluation;
 using NodaTime;
 
 namespace Ical.Net.CalendarComponents;
@@ -16,14 +17,14 @@ public interface IAlarmContainer
     /// </summary>
     ICalendarObjectList<Alarm> Alarms { get; }
 
-    ///  <summary>
-    ///  Polls <see cref="Alarm"/>s for occurrences within the <see cref="RecurringComponent.GetOccurrences"/>d
-    ///  time frame of this <see cref="RecurringComponent"/>.  For each evaluated
-    ///  occurrence if this component, each <see cref="Alarm"/> is polled for its
-    ///  corresponding alarm occurrences.
-    ///  </summary>
-    /// <param name="startTime">The earliest allowable alarm occurrence to poll, or <c>null</c>.</param>
-    /// <param name="endTime"></param>
-    /// <returns>A List of <see cref="AlarmOccurrence"/> objects, one for each occurrence of the <see cref="Alarm"/>.</returns>
-    IList<AlarmOccurrence> PollAlarms(DateTimeZone timeZone, Instant? startTime, Instant? endTime);
+    /// <summary>
+    /// Gets a streaming sequence of <see cref="AlarmOccurrence"/>s for all <see cref="Alarms"/>
+    /// on this component, with fire times in the range [<paramref name="startTime"/>, <paramref name="endTime"/>).
+    /// </summary>
+    /// <param name="timeZone">The time zone used to evaluate component occurrences.</param>
+    /// <param name="startTime">Lower bound (inclusive) on alarm fire times, or <c>null</c> for no lower bound.</param>
+    /// <param name="endTime">Upper bound (exclusive) on alarm fire times, or <c>null</c> for no upper bound.</param>
+    /// <param name="options">Evaluation options, or <c>null</c> for defaults.</param>
+    /// <returns>A streaming sequence of <see cref="AlarmOccurrence"/> objects.</returns>
+    IEnumerable<AlarmOccurrence> GetAlarmOccurrences(DateTimeZone timeZone, Instant? startTime = null, Instant? endTime = null, EvaluationOptions? options = null);
 }
