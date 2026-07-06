a1f2f565336dd7c3fa162128079eed77c584b35e
Mark Knapp <makman99@gmail.com>
2026-07-06 10:09:10 -0400

    Case-insensitive time zone provider
    Adds a case-insensitive time zone provider to more closely
    match previous behavior.

--- Ical.Net/CalendarTimeZoneProviders.cs
+++ Ical.Net/CalendarTimeZoneProviders.cs
@@ -21,6 +21,16 @@ public static class CalendarTimeZoneProviders
     /// </summary>
     public static readonly IDateTimeZoneProvider TzdbWithAliases = new InternalTzdbWithAliases();
 
+    /// <summary>
+    /// The same as <see cref="TzdbWithAliases"/> except time zone
+    /// lookups are case-insensitive. This is non-standard and should
+    /// not be used unless necessary.
+    /// </summary>
+    public static IDateTimeZoneProvider TzdbWithAliasesIgnoreCase => _tzdbWithAliasesIgnoreCase.Value;
+
+    private static readonly Lazy<InternalTzdbWithAliasesIgnoreCase> _tzdbWithAliasesIgnoreCase
+        = new(static () => new InternalTzdbWithAliasesIgnoreCase(), isThreadSafe: true);
+
     public static IDateTimeZoneProvider FromCalendar(Calendar calendar, params IDateTimeZoneProvider[] fallbackProviders)
     {
         var calendarProvider = new DateTimeZoneCache(calendar.TimeZoneSource);
@@ -150,6 +160,68 @@ public static class CalendarTimeZoneProviders
             }
 
             return p.GetZoneOrNull(id.Replace("-", "/"));
+        }
+    }
+
+    /// <summary>
+    /// The TZDB but allows time zone ID to match by common aliases
+    /// with case-insensitive matching.
+    /// </summary>
+    private sealed class InternalTzdbWithAliasesIgnoreCase : IDateTimeZoneProvider
+    {
+        private readonly Dictionary<string, string> _windowsMappingIgnoreCase =
+            TzdbDateTimeZoneSource.Default.WindowsMapping.PrimaryMapping
+                .ToDictionary(k => k.Key, v => v.Value, StringComparer.OrdinalIgnoreCase);
+
+        /// <summary>
+        /// TZDB ID lookup with case-insensitive matching. This would be better
+        /// as a HashSet, but netstandard2.0 does not allow getting the actual
+        /// value from the HashSet.
+        /// </summary>
+        private readonly Dictionary<string, string> _tzdbIgnoreCase =
+            DateTimeZoneProviders.Tzdb.Ids
+                .ToDictionary(x => x, x => x, StringComparer.OrdinalIgnoreCase);
+
+        public DateTimeZone this[string id]
+        {
+            get
+            {
+                var zone = GetZoneOrNull(id)
+                    ?? throw new DateTimeZoneNotFoundException($"Time zone ID {id} could not be found in TZDB");
+
+                return zone;
+            }
+        }
+
+        public string VersionId => DateTimeZoneProviders.Tzdb.VersionId;
+
+        /// <summary>
+        /// This returns the canonical TZDB list only.
+        /// </summary>
+        public ReadOnlyCollection<string> Ids => DateTimeZoneProviders.Tzdb.Ids;
+
+        public DateTimeZone GetSystemDefault() => DateTimeZoneProviders.Tzdb.GetSystemDefault();
+
+        public DateTimeZone? GetZoneOrNull(string id)
+        {
+            // RFC allows a prefixing "/" to indicate a "unique ID
+            // in a globally defined time zone registry." Ignore since
+            // this is already searching the TZDB.
+            if (id.StartsWith("/", StringComparison.Ordinal))
+            {
+                id = id.Substring(1);
+            }
+
+            DateTimeZone? zone = null;
+
+            if (_tzdbIgnoreCase.TryGetValue(id, out var tzdbId)
+                || _tzdbIgnoreCase.TryGetValue(id.Replace("-", "/"), out tzdbId)
+                || _windowsMappingIgnoreCase.TryGetValue(id, out tzdbId))
+            {
+                zone = DateTimeZoneProviders.Tzdb.GetZoneOrNull(tzdbId);
+            }
+
+            return zone;
         }
     }
 }
